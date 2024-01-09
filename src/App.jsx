73f92de4f018c5393af9f5fd4398cf4c84ca9f28
min.wan <min.wan@sinocare.com>
2024-01-09 10:43:18 +0800

    feat: 修改配置，并新增效果

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import confetti from "canvas-confetti";
 import { observer } from "mobx-react-lite";
 import { getSiteData } from "@/utils/getSiteData";
 import { GlobalScrollbar } from "mac-scrollbar";
@@ -14,15 +15,55 @@ const App = observer(() => {
   // 加载配置
   const apiKey = import.meta.env.VITE_API_KEY;
   const countDays = import.meta.env.VITE_COUNT_DAYS;
-
   // 获取站点数据
   const getSiteStatusData = () => {
     setSiteData(null);
     getSiteData(apiKey, countDays, cache, status).then((res) => {
       console.log(res);
       setSiteData(res);
+      setTimeout(() => {
+        confettiAnmaite();
+      }, 1000);
     });
   };
+
+  const confettiAnmaite = () => {
+    const count = 200;
+    const defaults = {
+      origin: { y: 0.6 }
+    };
+
+    function fire(particleRatio, opts) {
+      confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio)
+      });
+    }
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+    fire(0.2, {
+      spread: 60,
+    });
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2
+    });
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  }
 
   useEffect(() => {
     getSiteStatusData();
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -22,10 +22,6 @@ const Footer = () => {
       </div>
       <div className="text">
         <p>
-          <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
-          />
           &nbsp;Version&nbsp;{Package.version}
         </p>
         <p>
@@ -36,7 +32,6 @@ const Footer = () => {
         <p>
           Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
           &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
           {siteIcp ? (
             <React.Fragment>
               &nbsp;|&nbsp;
